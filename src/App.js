77e16c4dc101bba2834a1a30ad36a5202a859247
Manjunath K <mk@Manjunaths-MacBook-Pro.local>
2022-05-09 21:44:19 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ function App() {
   };
   const handleScroll = (event) => {
     let scrollTop = window.pageYOffset;
-    if (scrollTop > 7050) {
+    if (scrollTop > 7150) {
       // contact.current.style.height = "0px";
       contact.current.style.opacity = 0;
     } else {
