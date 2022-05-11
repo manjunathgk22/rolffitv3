223e824bbad84a8489bbf53131b29b194a393c4c
Manjunath K <mk@Manjunaths-MacBook-Pro.local>
2022-05-11 23:50:34 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -122,10 +122,10 @@ function App() {
           </main>
         </div>
       </FadeInWhenVisible>
-      <div onClick={gotocontact} ref={contact} className="lg:hidden contact-banner fixed bottom-0 left-0 w-screen p-3 bg-black flex z-30 justify-center items-center">
-        <BsFillTelephoneFill size={"1rem"} color={colors.white} /> <span className="text-white ml-4 text-lg font-geomanist tracking-wider">Get free demo</span>
+      <div onClick={gotocontact} ref={contact} className="lg:hidden contact-banner fixed bottom-0 left-0 w-screen p-3 bg-black flex z-30 justify-center items-center opacity-0">
+        <BsFillTelephoneFill size={"1rem"} color={colors.white} /> <span className="text-white ml-4 text-lg font-geomanist tracking-wider ">Get free demo</span>
       </div>
-      <div ref={contactlg} onClick={gotocontact} className="z-40 contact-float cursor-pointer invisible flex justify-center items-center lg:visible fixed bg-black p-4 bottom-4 right-4 rounded-full">
+      <div ref={contactlg} onClick={gotocontact} className="z-40 contact-float cursor-pointer invisible flex justify-center items-center lg:visible fixed bg-black p-4 bottom-4 right-4 rounded-full opacity-0">
         <BsFillTelephoneFill size={"1.25rem"} color={colors.white} /> <span className="text-white ml-4 text-lg font-geomanist tracking-wider">Get free demo</span>
       </div>
     </>
